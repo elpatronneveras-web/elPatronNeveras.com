d7c8330525ce49b0bfb18e920af0ec313d038f7e
elpatronneveras-web <elpatron.neveras@gmail.com>
2025-11-12 22:54:59 -0400

    Add files via upload

---
+++ elPatronNeveras.js
@@ -0,0 +1,11 @@
+// script.js
+
+// Mobile menu toggle
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+if (menuToggle) {
+  menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+  });
+}
